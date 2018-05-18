1b4551f8b7bb2f556a7e3115bc1262fad45ebb24
iandominey <ian.dominey@gmail.com>
2018-05-18 21:53:36 +0100

    Fixed TodoTextInput

--- test/app/components/TodoTextInput.spec.js
+++ test/app/components/TodoTextInput.spec.js
@@ -3,6 +3,8 @@ import { mount } from 'enzyme';
 import TodoTextInput from '../../../app/components/TodoTextInput';
 import style from '../../../app/components/TodoTextInput.css';
 
+const getInput = renderer => renderer.childAt(0);
+
 function setup(propOverrides) {
   const props = {
     onSave: jest.fn(),
@@ -15,58 +17,58 @@ function setup(propOverrides) {
 
 
   const renderer = mount(<TodoTextInput {...props} />);
+  const input = getInput(renderer);
 
-  return { props, renderer };
+  return { props, renderer, input };
 }
 
 describe('todoapp TodoTextInput component', () => {
   it('should render correctly', () => {
-    const { renderer } = setup();
-    const input = renderer.childAt(0);
+    const { input } = setup();
     expect(input.prop('placeholder')).toBe('What needs to be done?');
     expect(input.prop('value')).toBe('Use Redux');
     expect(input.prop('className')).toBe('');
   });
 
   it('should render correctly when editing=true', () => {
-    const { renderer } = setup({ editing: true });
-    expect(renderer.hasClass(style.edit)).toBe(true);
+    const { input } = setup({ editing: true });
+    expect(input.hasClass(style.edit)).toBe(true);
   });
 
   it('should render correctly when newTodo=true', () => {
-    const { renderer } = setup({ newTodo: true });
-    expect(renderer.hasClass(style.new)).toBe(true);
+    const { input } = setup({ newTodo: true });
+    expect(input.hasClass(style.new)).toBe(true);
   });
 
   it('should update value on change', () => {
-    const { renderer } = setup();
-    const input = renderer.first();
-    input.simulate('change', { target: { value: 'Use Radox' } });
-    expect(input.prop('value')).toBe('Use Radox');
+    const { renderer, input } = setup();
+    input.prop('onChange')({ target: { value: 'Use Radox' } });
+    renderer.update();
+    expect(getInput(renderer).prop('value')).toBe('Use Radox');
   });
 
   it('should call onSave on return key press', () => {
-    const { renderer, props } = setup();
-    renderer.props.onKeyDown({ which: 13, target: { value: 'Use Redux' } });
+    const { input, props } = setup();
+    input.prop('onKeyDown')({ which: 13, target: { value: 'Use Redux' } });
     expect(props.onSave).toBeCalledWith('Use Redux');
   });
 
   it('should reset state on return key press if newTodo', () => {
-    const { renderer } = setup({ newTodo: true });
-    renderer.props.onKeyDown({ which: 13, target: { value: 'Use Redux' } });
-    const updated = renderer.getRenderrenderer();
-    expect(updated.props.value).toBe('');
+    const { renderer, input } = setup({ newTodo: true });
+    input.prop('onKeyDown')({ which: 13, target: { value: 'Use Redux' } });
+    renderer.update();
+    expect(getInput(renderer).prop('value')).toBe('');
   });
 
   it('should call onSave on blur', () => {
-    const { renderer, props } = setup();
-    renderer.props.onBlur({ target: { value: 'Use Redux' } });
+    const { input, props } = setup();
+    input.prop('onBlur')({ target: { value: 'Use Redux' } });
     expect(props.onSave).toBeCalledWith('Use Redux');
   });
 
-  it('shouldnt call onSave on blur if newTodo', () => {
-    const { renderer, props } = setup({ newTodo: true });
-    renderer.props.onBlur({ target: { value: 'Use Redux' } });
-    expect(props.onSave.calls.length).toBe(0);
+  it('shouldn\'t call onSave on blur if newTodo', () => {
+    const { input, props } = setup({ newTodo: true });
+    input.prop('onBlur')({ target: { value: 'Use Redux' } });
+    expect(props.onSave).not.toBeCalled();
   });
 });
